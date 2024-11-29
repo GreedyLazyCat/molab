f3f4660169afbfed18a0ebb921fe66bf330600d1
Dima Chukhlancev <htmlweb78@gmail.com>
2024-11-29 21:31:25 +0300

    Some changes in lib

--- molib/lib/molib.dart
+++ molib/lib/molib.dart
@@ -4,6 +4,6 @@
 library;
 
 export 'src/fraction/fraction.dart' show Fraction;
-export 'src/artificial_solver.dart' show ArtificialSolver, MatrixMode;
+export 'src/artificial_solver.dart' show ArtificialSolver, MatrixMode, StepMatrix;
 export 'src/mx_entry.dart' show MxEntry;
 
