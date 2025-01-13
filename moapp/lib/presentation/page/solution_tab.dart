671179bc4ee9cdd3ab2363ead8512bfd3832d377
Dima Chukhlancev <htmlweb78@gmail.com>
2025-01-13 17:59:50 +0300

    Added point display for an answer

--- moapp/lib/presentation/page/solution_tab.dart
+++ moapp/lib/presentation/page/solution_tab.dart
@@ -14,6 +14,22 @@ class SolutionTab extends StatefulWidget {
 }
 
 class _SolutionTabState extends State<SolutionTab> {
+  String generateSolvedPointString(StepInfo step) {
+    if (step.type != StepType.solved) {
+      return "";
+    }
+    final point = List<dynamic>.generate(
+        step.colIndices.length + step.rowIndices.length, (index) => 0);
+    var row = 0;
+    for (var i in step.colIndices) {
+      point[i - 1] = step.stepMatrix[row][step.stepMatrix.first.length - 1];
+      row += 1;
+    }
+    return point
+        .map((e) => (e is Fraction) ? e.reduced().toString() : e.toString())
+        .toString();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -34,8 +50,9 @@ class _SolutionTabState extends State<SolutionTab> {
       return const Text("Нет задачи");
     }
     final lastStep = controller.solver!.lastStep;
+
     final solvedText = (lastStep.type == StepType.solved)
-        ? "Ответ: f*=${(lastStep.stepMatrix.last.last * -1).toString()}"
+        ? "Ответ: f*=${(lastStep.stepMatrix.last.last * -1).toString()} x*=${generateSolvedPointString(lastStep)}"
         : "Решение в процессе";
 
     return Container(
